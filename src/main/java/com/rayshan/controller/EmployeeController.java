caa1914391255074d23070eaf7ab23eeb09bc0ec
noushadbm <noushadbm@gmail.com>
2023-02-13 23:15:15 +0400

    Added logs.

--- src/main/java/com/rayshan/controller/EmployeeController.java
+++ src/main/java/com/rayshan/controller/EmployeeController.java
@@ -2,10 +2,10 @@ package com.rayshan.controller;
 
 import com.rayshan.common.entities.Employee;
 import com.rayshan.exception.ResourceNotFoundException;
-import com.rayshan.repository.EmployeeRepository;
 import com.rayshan.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
     private EmployeeService employeeService;
 
     @Autowired
@@ -22,18 +23,20 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
+        logger.info("Get all employees API invoked.");
         return this.employeeService.getAllEmployees();
     }
 
     @GetMapping("/employees/{id}")
     public Employee getAllEmployeeById(@PathVariable(value = "id") Long employeeId) throws ResourceNotFoundException{
+        logger.info("Get all employee by id API invoked. Employee ID: {}", employeeId);
         Employee employee = this.employeeService.getAllEmployeeById(employeeId);
         return employee;
     }
 
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
-        System.out.println("Request reached here...");
+        logger.info("Create employee API invoked. Employee name: {} {}", employee.getFirstName(), employee.getLastName());
         return this.employeeService.createEmployee(employee);
     }
 }
--- src/main/java/com/rayshan/listeners/AuthSuccessEventListener.java
+++ src/main/java/com/rayshan/listeners/AuthSuccessEventListener.java
@@ -2,6 +2,8 @@ package com.rayshan.listeners;
 
 import com.rayshan.common.entities.LoginActivity;
 import com.rayshan.repository.LoginActivityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
@@ -12,6 +14,7 @@ import java.util.Date;
 
 @Component
 public class AuthSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
+    Logger logger = LoggerFactory.getLogger(AuthSuccessEventListener.class);
     private LoginActivityRepository activityRepo;
 
     @Autowired
@@ -20,12 +23,13 @@ public class AuthSuccessEventListener implements ApplicationListener<Authenticat
     }
     @Override
     public void onApplicationEvent(AuthenticationSuccessEvent event) {
-        System.out.println("Yes it is a success..." + event.getAuthentication().getPrincipal());
+        //System.out.println("Yes it is a success..." + event.getAuthentication().getPrincipal());
         LoginActivity activity = new LoginActivity();
         User user = (User)event.getAuthentication().getPrincipal();
         activity.setUserName(user.getUsername());
         activity.setDate(new Date());
         activity.setStatus("SUCCESS");
+        logger.info("User logged in successfully. User: {}", user.getUsername());
         activityRepo.save(activity);
     }
 }
