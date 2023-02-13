a76708cd552345e99d33886bd9e3d2ea3cd009af
noushadbm <noushadbm@gmail.com>
2023-02-13 23:20:46 +0400

    Added logs.

--- src/main/java/com/rayshan/listeners/AuthFailureEventListener.java
+++ src/main/java/com/rayshan/listeners/AuthFailureEventListener.java
@@ -2,6 +2,8 @@ package com.rayshan.listeners;
 
 import com.rayshan.common.entities.LoginActivity;
 import com.rayshan.repository.LoginActivityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
@@ -11,6 +13,7 @@ import java.util.Date;
 
 @Component
 public class AuthFailureEventListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+    Logger logger = LoggerFactory.getLogger(AuthFailureEventListener.class);
     private LoginActivityRepository activityRepo;
 
     @Autowired
@@ -19,13 +22,12 @@ public class AuthFailureEventListener implements ApplicationListener<Authenticat
     }
     @Override
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
-        //ApplicationListener<AuthenticationSuccessEvent>
-        System.out.println("Yes it is a failure..." + event.getAuthentication().getPrincipal());
+        //System.out.println("Yes it is a failure..." + event.getAuthentication().getPrincipal());
         LoginActivity activity = new LoginActivity();
         activity.setUserName(event.getAuthentication().getPrincipal().toString());
         activity.setDate(new Date());
         activity.setStatus("FAILURE");
+        logger.info("Login failed because of wrong credentials. User name: {}", activity.getUserName());
         activityRepo.save(activity);
-
     }
 }
