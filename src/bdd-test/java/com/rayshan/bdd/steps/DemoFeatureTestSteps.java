4c11c8cc5081808a6e7c9ed572767d8fd39f2268
noushadbm <noushadbm@gmail.com>
2023-02-18 14:41:50 +0400

    Updated BDD step.

--- src/bdd-test/java/com/rayshan/bdd/steps/DemoFeatureTestSteps.java
+++ src/bdd-test/java/com/rayshan/bdd/steps/DemoFeatureTestSteps.java
@@ -8,33 +8,40 @@ import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Disabled;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled
 public class DemoFeatureTestSteps extends BaseIntegrationTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoFeatureTestSteps.class);
+    private static final String SERVICE_BASE_URL = "http://localhost:8080";
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Given("^Service is up and running$")
-    public void some_service_exists() throws Throwable {
+    public void some_service_exists() {
         LOGGER.info(() -> "++ GIVEN -> some_service_exists()");
+        String response = restTemplate.getForObject(SERVICE_BASE_URL + "/api/test/hello", String.class);
+        assertEquals("Hello world", response);
     }
 
     @And("^setup clean & fresh test data$")
-    public void setup_clean_fresh_test_data() throws Throwable {
+    public void setup_clean_fresh_test_data() {
         LOGGER.info(() -> "++ AND -> setup_clean_fresh_test_data()");
     }
 
     @When("^the client calls service$")
-    public void client_calls_service() throws Throwable {
+    public void client_calls_service() {
         LOGGER.info(() -> "++ WHEN -> client_calls_service()");
     }
 
     @Then("^verify expected result$")
-    public void verify_expected_result() throws Throwable {
+    public void verify_expected_result() {
         LOGGER.info(() -> "++ THEN -> verify_expected_result()");
     }
 
     @Then("^cleanup demo test data$")
-    public void cleanup_test_data() throws Throwable {
+    public void cleanup_test_data() {
         LOGGER.info(() -> "++ THEN -> cleanup_test_data()");
     }
 
